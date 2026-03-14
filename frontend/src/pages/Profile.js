4a3e448421279418591bcde9a6aee24c8dec0981
KeerthiReddyGudibandi14 <reddygudibandi798@gmail.com>
2026-03-14 10:55:18 -0400

    Implemented User Profile Page with Booking History (#19)

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,3 +1,5 @@
+
+// Issue #19: User Profile Page with Booking History
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { userAPI } from '../services/api';
